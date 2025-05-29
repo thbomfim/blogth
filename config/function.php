c48832d5bea40c7a9ccbe485f5e6802b9d1a78e0
Thiago Bomfim <40004059+thbomfim@users.noreply.github.com>
2025-05-28 21:55:30 -0300

    Update function.php
    add funcao para pegar o nome do usuario

--- config/function.php
+++ config/function.php
@@ -2,4 +2,19 @@
 function islogado() {
     
 }
+/////////////////////mostra o nome do usuario
+function nameUser() 
+{
+    global $pdo;
+
+    $sql = "SELECT nome FROM usuarios WHERE id = :iduser";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(":iduser", $_SESSION["id"]);
+    $stmt->execute();
+
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    return $row["nome"];
+}
+
 ?>
