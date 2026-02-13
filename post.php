97dfe436756f51865682aeb9f5b012a8ff1660ab
Thiago Bomfim <40004059+thbomfim@users.noreply.github.com>
2026-02-12 23:37:22 -0300

    mudei a forma como mostra o post
    tentei deixar um pouco mais organizado pois antes tinha código html na classe posts  assim ficou menos bagunçado separando um pouco

--- post.php
+++ post.php
@@ -1,26 +1,28 @@
 <?php
 include("header.html");
 include("config/config.php");
+include("Classes/Post.class.php");
+
 
     if ($pg == "post") {
-        $id = $_GET['id'];
-
-        $sql = "SELECT * FROM posts WHERE id = :id";
-        $stmt = $pdo->prepare($sql);
-        $stmt->bindParam(':id', $id);
-        $stmt->execute();
-        $post = $stmt->fetch();
-?>
-        <br><h1><?php echo $post['titulo']; ?></h1>
-        <br><h3><?php echo $post['intro']; ?></h3>
-        <br><blockquote class="author"><?php echo $post['autor']; ?> <?php echo $post['dataCriado']; ?></blockquote>
-        </div>
+        $post = new Post($pdo);
+        ?>
+        
+        <br><h1><?= $post->mostrarTitulo(); ?></h1><br>
+        <blockquote class="author"><?= $post->mostrarAutor(); ?> <?= $post->mostrarDataCriado() ?></blockquote><br>
+        <h3><?= $post->mostrarIntro(); ?></h3><br>
+    </div>
         <div class="container-md meu-container">
-        <p class="capitalize"><?php echo $post['conteudo']; ?></p>
+        <!-- tenho que fazer um tratamento no objeto mostrarConteudo para mostrar somente a proxima palvra depois da intro -->
+        <p class="capitalize"><?= $post->mostrarConteudo() ?></p>
         <?php } else { ?>
         <p>Este post não existe.</p>
-    <?php } ?>
-    <button type="submit" class="btn btn-primary" onclick="window.history.back()">Voltar</button>
+
+        <?php
+    }
+
+?>
+    <center><button type="submit" class="btn btn-primary" onclick="window.history.back()">Voltar</button></center>
 <?php
 include("footer.html");
 ?>
